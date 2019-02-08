dfa95688521cd8acd4fb67894678d4399ca2ee1b
draffensperger <daveraff@google.com>
2019-02-08 15:20:50 -0500

    Add empty test to make the karma build pass

--- packages/opencensus-web-instrumentation-perf/test/index.ts
+++ packages/opencensus-web-instrumentation-perf/test/index.ts
@@ -13,3 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+describe('the empty library', () => {
+  it('does nothing', () => {});
+});
